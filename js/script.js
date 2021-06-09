4d0b7bcfa4c721b2271d36cd129ac637f9b37c4f
MyUsername <emanuele.marini37@gmail.com>
2021-06-09 21:49:49 +0200

    bozza palindromi

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
 //Palidroma
 //Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+// ripetizione recap pomeridiano Andrea
 
+//inserimento parola richiesta con prompt
+
+var parolaUtente = prompt('Inserisci una parola');
+var lunghezzaParola = parolaUtente.length;
+
+alert(parolaUtente)
+
+//l idea è quella di richiamare tramite [] ogni singola lettera
+//della parola che in sostanza ad ogni istanza [0], [1] e cosi via 
+// alert(parolaUtente)[3]
+// alert(parolaUtente)[2]
+// alert(parolaUtente)[1]
+// alert(parolaUtente)[1]
+//ci restituirebbe la parola come una stringa di lettere 
+//con un for cicliamo il meccanismo ..al contrario dall ultima
+
+
+// la i però sarà la lunghezza della parola
+for (var i = (lunghezzaParola - 1); i >=0; i++)
+{
+      alert(parolaUtente[i])
+}
 
 
 
@@ -13,3 +36,11 @@
 // Sommiamo i due numeri.
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Infine, dichiariamo chi ha vinto.
+
+
+function getRandomNumber(){
+             var randomNumber = Math.floor(Math.random()* 5) + 1;
+             return randomNumber;
+       }
+
+var userNumber = (prompt('Inserisci un numero')); 
